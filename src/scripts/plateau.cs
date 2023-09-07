ac283dfe8415eb448e4e56a7322d4501413de392
kcastel <keviniicastel@gmail.com>
2023-09-07 20:33:53 +0200

    ajout des fichiers

---
+++ src/scripts/plateau.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+public partial class plateau : Node2D
+{
+	
+	Camera2D camObject;
+	
+	Marker2D makerPlayer1;
+	
+	enum CameraStates
+	{
+		MOVE,
+		ZOOM,
+		UNZOOM
+	}
+	
+	CameraStates camState = CameraStates.MOVE;
+	
+	const ushort MAXIMUM_SPEED = 20;
+	const ushort ACCELERATION = 20;
+	
+	float CamSpeed = 0.00f;
+	
+	
+	Vector2 go_to = new Vector2(0,0);
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.camObject = GetNode<Camera2D>("cam2D");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		this.CameraStateMachine();
+	}
+	
+	private void CameraMovement(){
+		/*
+			Cette méthode est appelée pour les déplacements de caméra
+		*/
+		if (this.camObject.GlobalPosition.Y > this.go_to.Y){
+			this.CamSpeed = Mathf.;
+		} else if (this.camObject.GlobalPosition.Y < this.go_to.Y) {
+			
+		}
+	}
+	
+	private void CameraStateMachine(){
+		switch (camState) {
+			case CameraStates.MOVE:
+				this.CameraMovement();
+				break;
+			default:
+				break;
+		}
+		if (this.camObject.GlobalPosition.X > 2){
+			
+		}
+	}
+}
---
+++ src/scripts/terrainproto.gd
@@ -0,0 +1,82 @@
+extends Node2D
+
+var STATE_CAM
+enum {MOVE, ZOOM, UNZOOM}
+
+var ACTION
+enum {PLAYER1,PLAYER2, CONFRONTATION}
+
+var move_from = Vector2.ZERO
+var move_to = Vector2.ZERO
+
+var _speed_cam = 0
+
+const MAXIMUM_SPEED = 10
+const ACCELRATION = 4
+const MAX_ZOOM = 1
+const MIN_ZOOM = 0.6
+const SPEED_ZOOM = 0.025
+
+@onready var _cam = self.get_node("Camera2D")
+
+func _ready():
+	self.ACTION = PLAYER1
+	self.SetMove(MOVE,"player1")
+
+
+func _physics_process(_delta):
+	self.StateMachine()
+
+func SetMove(new_state, destination):
+	self.STATE_CAM = new_state
+	match destination:
+		"player1":
+			self.move_to = self.get_node("Marker2DPlayer1").global_position
+		"player2":
+			self.move_to = self.get_node("Marker2DPlayer2").global_position
+		"confrontation":
+			self.move_to = self.get_node("Marker2DPlayerConfrontation").global_position
+
+
+func StateMachine():
+	if self.STATE_CAM == MOVE:
+		self.move_from = self._cam.global_position
+		if move_from.y < move_to.y+2:
+			self._speed_cam = min(self._speed_cam+self.ACCELRATION, self.MAXIMUM_SPEED)
+		elif move_from.y > move_to.y-2:
+			self._speed_cam = max(self._speed_cam-self.ACCELRATION, -self.MAXIMUM_SPEED)
+		
+		if move_from.y > move_to.y - 2 and move_from.y < move_to.y + 2:
+			self._speed_cam = 0.00
+			self.STATE_CAM = ZOOM
+		else:
+			self._speed_cam = lerpf(self._speed_cam, 0.00,0.15)
+		
+		self.move_from.y += self._speed_cam
+		self._cam.global_position = self.move_from
+	elif self.STATE_CAM == ZOOM:
+		if self._cam.zoom.x < self.MAX_ZOOM:
+			self._cam.zoom.x = min(self._cam.zoom.x+self.SPEED_ZOOM, self.MAX_ZOOM)
+			self._cam.zoom.y = self._cam.zoom.x
+		else:
+			self.STATE_CAM = UNZOOM
+	elif self.STATE_CAM == UNZOOM:
+		if self._cam.zoom.x > self.MIN_ZOOM:
+			self._cam.zoom.x = max(self._cam.zoom.x-self.SPEED_ZOOM, self.MIN_ZOOM)
+			self._cam.zoom.y = self._cam.zoom.x
+			if self._cam.zoom.x < self.MIN_ZOOM+0.01:
+				self.ChangeCam()
+
+
+func ChangeCam():
+	match self.ACTION:
+		PLAYER1:
+			self.ACTION = PLAYER2
+			self.SetMove(MOVE, "player2")
+		PLAYER2:
+			self.ACTION = CONFRONTATION
+			self.SetMove(MOVE, "confrontation")
+		CONFRONTATION:
+			self.ACTION = PLAYER1
+			self.SetMove(MOVE, "player1")
+
